055f1fa7eebb680cf98dc2812f4f07a1d79cf2ab
Ralf Geschke <ralf@kuerbis.org>
2022-07-26 18:39:03 +0200

    Some cleanup, add comments, remove debug output

--- pkg/daemon.go
+++ pkg/daemon.go
@@ -1,3 +1,7 @@
+// Copyright 2022 Ralf Geschke. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package pkg
 
 import (
@@ -24,8 +28,10 @@ type CollectDaemon struct {
 	lib      *golrackpi.AuthClient
 }
 
+// PdvMap defines a map of ProcessDataValue type with a string as key
 type PdvMap map[string]golrackpi.ProcessDataValue
 
+// convertPdvMap converts a slice of ProcessDataValue into a map of ProcessDataValue type with the Id as key
 func convertPdvMap(pdv []golrackpi.ProcessDataValue) PdvMap {
 	pdvmap := make(PdvMap)
 
@@ -35,6 +41,7 @@ func convertPdvMap(pdv []golrackpi.ProcessDataValue) PdvMap {
 	return pdvmap
 }
 
+// convertPdvsMap converts a slice of ProcessDataValues into a map of PdvMap type with the ModuleId as key
 func convertPdvsMap(pdvs []golrackpi.ProcessDataValues) map[string]PdvMap {
 	pdvsmap := make(map[string]PdvMap)
 	for i := range pdvs {
@@ -44,21 +51,21 @@ func convertPdvsMap(pdvs []golrackpi.ProcessDataValues) map[string]PdvMap {
 	return pdvsmap
 }
 
+// convertProcessDataValues converts a slice of processdata values into JSON and returns the result as slice of bytes, which can be stored in the MariaDB database
 func convertProcessDataValues(pd []golrackpi.ProcessDataValues) ([]byte, error) {
-
 	var pdvsJSON []byte
 	pdvsmap := convertPdvsMap(pd)
 	pdvsJSON, err := json.Marshal(pdvsmap)
 	if err != nil {
 		return pdvsJSON, err
 	}
-
-	//repository.AddData(string(pdvsJSON))
 	return pdvsJSON, nil
 }
 
+// innerLoop gets, converts and stores inverter values every tickerTimeSeconds seconds. It runs a duration of newLoginTimeMinutes,
+// then the timer and ticker are stopped.
 func (cd *CollectDaemon) innerLoop(ctx context.Context, newLoginTimeMinutes int64, tickerTimeSeconds int64) {
-	log.Println("in innerLoop")
+	log.Println("start innerLoop")
 
 	timer2 := time.NewTimer(time.Duration(newLoginTimeMinutes) * time.Minute)
 	ticker := time.NewTicker(time.Duration(tickerTimeSeconds) * time.Second)
@@ -66,8 +73,10 @@ func (cd *CollectDaemon) innerLoop(ctx context.Context, newLoginTimeMinutes int6
 	// todo: if something failed, repeat 5 or 10 times, then set fail status
 	for active := true; active; {
 		select {
-		case t := <-ticker.C:
-			fmt.Println("Tick at", t)
+		//case t := <-ticker.C:
+		case <-ticker.C:
+
+			//fmt.Println("Tick at", t)
 
 			pd, err := cd.lib.ProcessDataValues(collectProcessData)
 
@@ -82,12 +91,13 @@ func (cd *CollectDaemon) innerLoop(ctx context.Context, newLoginTimeMinutes int6
 				// fail silently
 				continue
 			}
-			lastId, err := repository.AddData(string(pdvsJSON))
+			//lastId, err := repository.AddData(string(pdvsJSON))
+			_, err = repository.AddData(string(pdvsJSON))
 			if err != nil {
 				// fail silently
 				continue
 			}
-			fmt.Println("entry", lastId, "added")
+			//log.Println("entry", lastId, "added")
 
 		case <-timer2.C:
 			log.Println("timer2 fired")
@@ -108,6 +118,8 @@ func (cd *CollectDaemon) innerLoop(ctx context.Context, newLoginTimeMinutes int6
 
 }
 
+// outerLoop logs into inverter and opens a database connection. Then it gives control to innerLoop, which gets ProcessDataValues.
+// After newLoginTimeMinutes a logout and connection close is performed, so the loop starts again. The whole process can be stopped by a context signal.
 func (cd *CollectDaemon) outerLoop(ctx context.Context, newLoginTimeMinutes int64, tickerTime int64) {
 	//timer1 := time.NewTimer(10 * time.Hour)
 
@@ -125,13 +137,13 @@ func (cd *CollectDaemon) outerLoop(ctx context.Context, newLoginTimeMinutes int6
 				cd.PrintMemUsage()
 				err := cd.login()
 				if err != nil {
-					fmt.Println(err)
+					log.Println(err)
 					panic("hard error 1")
 				}
 				log.Println("in for cnt:", cnt, " and before innerLoop", time.Now())
 				err = cd.openDbRepository()
 				if err != nil {
-					fmt.Println(err)
+					log.Println(err)
 					panic("hard error 4")
 				}
 
@@ -139,12 +151,12 @@ func (cd *CollectDaemon) outerLoop(ctx context.Context, newLoginTimeMinutes int6
 				log.Println("after innerLoop", time.Now())
 				err = cd.logout()
 				if err != nil {
-					fmt.Println(err)
+					log.Println(err)
 					panic("hard error 2") // todo error handling
 				}
 				err = cd.closeDbRepository()
 				if err != nil {
-					fmt.Println(err)
+					log.Println(err)
 					panic("hard error 3")
 				}
 
@@ -173,46 +185,43 @@ func (cd *CollectDaemon) outerLoop(ctx context.Context, newLoginTimeMinutes int6
 
 	}
 	log.Println("outerLoop ends normal")
-	return
 }
 
+// logout performs a logout from inverter
 func (cd *CollectDaemon) logout() error {
 	ok, err := cd.lib.Logout()
 	if err != nil {
-		//fmt.Println("logout error", err)
 		return fmt.Errorf("logout error: %s", err)
 	}
-	fmt.Println("logout ok?", ok)
+	log.Println("logout ok?", ok)
 	return nil
 }
 
+// login performs a login into the inverter
 func (cd *CollectDaemon) login() error {
-	fmt.Println("Try another login...")
+	log.Println("Try another login...")
 	cd.lib = golrackpi.NewWithParameter(cd.AuthData)
-
-	fmt.Println(cd.lib.SessionId)
 
 	sessionId, err := cd.lib.Login()
 	if err != nil {
-		fmt.Println("An error occurred:", err)
+		log.Println("An error occurred:", err)
 		return fmt.Errorf("login error: %s", err)
 	}
-	fmt.Println("SessionId", sessionId)
-
+	log.Println("Login ok with SessionId", sessionId)
 	return nil
-
 }
 
+// openDbRepository opens the database connection
 func (cd *CollectDaemon) openDbRepository() error {
 	db, err := dbconn.ConnectDB(cd.DbConfig)
 	if err != nil {
 		return err
 	}
-
 	repository = invdb.NewRepository(db)
 	return nil
 }
 
+// closeDbRepository closes the database connection
 func (cd *CollectDaemon) closeDbRepository() error {
 	err := repository.Close()
 	if err != nil {
@@ -221,6 +230,7 @@ func (cd *CollectDaemon) closeDbRepository() error {
 	return nil
 }
 
+// Start takes configuration parameters and starts the data collecting process
 func (cd *CollectDaemon) Start(configProcessData []golrackpi.ProcessData, newLoginTimeMinutes int64, tickerTimeSeconds int64) {
 
 	cd.lib = golrackpi.NewWithParameter(cd.AuthData)
@@ -246,12 +256,13 @@ func (cd *CollectDaemon) Start(configProcessData []golrackpi.ProcessData, newLog
 
 	cd.outerLoop(ctx, newLoginTimeMinutes, tickerTimeSeconds)
 
-	log.Println("after outerLoop")
+	log.Println("Daemon stopped")
 
 	cd.PrintMemUsage()
 
 }
 
+// PrintMemUsage writes some memory statistics of the running Go process
 // https://golangcode.com/print-the-current-memory-usage/
 func (cd *CollectDaemon) PrintMemUsage() {
 	var m runtime.MemStats
